f3a190700a6d83303ee29e53686fd29198cd4c63
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2024-03-15 12:46:15 +0700

    onClickListener applied and data input could be read in the 3rd button

--- app/src/main/java/com/example/weatherapiapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapiapp/MainActivity.java
@@ -3,12 +3,52 @@ package com.example.weatherapiapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    //** Get/list the components
+    //* The component names don't have to be the same/match the button IDs/names in the xml layout */
+    Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
+    EditText et_dataInput;
+    ListView lv_weatherReport;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //* Assign values to each control/components on the layout
+        btn_cityID = findViewById(R.id.btn_getCityID);
+        btn_getWeatherByID = findViewById(R.id.btn_getWeatherByCityID);
+        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+
+        et_dataInput = findViewById(R.id.et_dataInput);
+        lv_weatherReport = findViewById(R.id.lv_weatherReports);
+
+        //* Click listeners for each button
+        btn_cityID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked me", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked me 2", Toast.LENGTH_SHORT).show();
+            }
+        });
+        
+        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You typed: " + et_dataInput.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
