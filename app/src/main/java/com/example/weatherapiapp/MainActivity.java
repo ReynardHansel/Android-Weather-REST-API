933d905318be1cccc5e6c1ca01323d8f4af3c52a
Reynard Hansel <92787298+ReynardHansel@users.noreply.github.com>
2024-03-31 18:32:44 +0700

    Changed the app concept from weather api to dog api because the weather api seems like to no longer work (other weather apis require sign up so, no)
    - Installed glide library for image loading
    - Added Image view to display the dog image
    - Modified the button texts and id to match the new concept
    - "Get Dog" button alread works (including fetching random image and specific user-inputted breed)

--- app/src/main/java/com/example/weatherapiapp/MainActivity.java
+++ app/src/main/java/com/example/weatherapiapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -13,15 +14,20 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     //** Get/list the components
     //* The component names don't have to be the same/match the button IDs/names in the xml layout */
-    Button btn_cityID, btn_getWeatherByID, btn_getWeatherByName;
+    Button btn_getDog, btn_getBreedInfo, btn_getBreedList;
     EditText et_dataInput;
-    ListView lv_weatherReport;
+    ListView lv_breedList;
+    ImageView iv_dogImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,51 +35,81 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //* Assign values to each control/components on the layout
-        btn_cityID = findViewById(R.id.btn_getCityID);
-        btn_getWeatherByID = findViewById(R.id.btn_getWeatherByCityID);
-        btn_getWeatherByName = findViewById(R.id.btn_getWeatherByCityName);
+        btn_getDog = findViewById(R.id.btn_getDog);
+        btn_getBreedInfo = findViewById(R.id.btn_getBreedInfo);
+        btn_getBreedList = findViewById(R.id.btn_getBreedList);
 
         et_dataInput = findViewById(R.id.et_dataInput);
-        lv_weatherReport = findViewById(R.id.lv_weatherReports);
+        lv_breedList = findViewById(R.id.lv_breedList);
+        iv_dogImage = findViewById(R.id.iv_dogImage);
+
+        // String url = "";
 
         //* Click listeners for each button
-        btn_cityID.setOnClickListener(new View.OnClickListener() {
+        btn_getDog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 // Instantiate the RequestQueue.
                 RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
-                String url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41";
 
-                // Request a string response from the provided URL.
-                StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                        new Response.Listener<String>() {
-                            @Override
-                            public void onResponse(String response) {
-                                // Display the first 500 characters of the response string.
-                                Toast.makeText(MainActivity.this, response, Toast.LENGTH_SHORT).show();
-                            }
-                        }, new Response.ErrorListener() {
+                String url;
+                String input = et_dataInput.getText().toString();
+
+                if (input.isEmpty()) {
+                    url = "https://dog.ceo/api/breeds/image/random";
+                } else {
+                    url = "https://dog.ceo/api/breed/" + input + "/images/random";
+                }
+
+                JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>(){
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        
+                        try {
+                            String imageUrl = response.getString("message");
+                            Glide.with(MainActivity.this).load(imageUrl).into(iv_dogImage);
+                            Toast.makeText(MainActivity.this, imageUrl, Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                            throw new RuntimeException(e);
+                        }
+
+                        // try {
+                        //     double latitude = response.getDouble("latitude");
+                        //     double longitude = response.getDouble("longitude");
+                
+                        //     Toast.makeText(MainActivity.this, "Latitude: " + latitude + ", Longitude: " + longitude, Toast.LENGTH_SHORT).show();
+                        // } catch (JSONException e) {
+                        //     e.printStackTrace();
+                        //     throw new RuntimeException(e);
+                        // }
+
+                        // Toast.makeText(MainActivity.this, response.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                }, new Response.ErrorListener() {
+
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Toast.makeText(MainActivity.this, "Error Occurred", Toast.LENGTH_SHORT).show();
+                        // TODO: Handle error
+                        Toast.makeText(MainActivity.this,"Dog Breed not found",Toast.LENGTH_SHORT).show();
                     }
                 });
 
-                // Add the request to the RequestQueue.
-                queue.add(stringRequest);
+                queue.add(request);
+
 //                Toast.makeText(MainActivity.this, "You clicked me", Toast.LENGTH_SHORT).show();
             }
         });
         
-        btn_getWeatherByID.setOnClickListener(new View.OnClickListener() {
+        btn_getBreedInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "You clicked me 2", Toast.LENGTH_SHORT).show();
             }
         });
         
-        btn_getWeatherByName.setOnClickListener(new View.OnClickListener() {
+        btn_getBreedList.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "You typed: " + et_dataInput.getText().toString(), Toast.LENGTH_SHORT).show();
